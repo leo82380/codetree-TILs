ff3e215fa6715fbdc40bd3c1058c6c4d99316924
LEE SANG GYU <tkdrb062@gmail.com>
2024-08-08 15:30:13 +0900

    Title: 알파벳 지우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/알파벳 지우기/remove-alphabet.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    string b;
+    string c;
+    string d;
+    cin >> a;
+    cin >> b;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (a[i] > 'a' && a[i] < 'z') continue;
+        c += a[i];
+    }
+    for (int i = 0; i < b.length(); i++)
+    {
+        if (b[i] > 'a' && b[i] < 'z') continue;
+        d += b[i];
+    }
+
+    int firstNum = stoi(c);
+    int secondNum = stoi(d);
+    cout << firstNum + secondNum;
+    return 0;
+}
